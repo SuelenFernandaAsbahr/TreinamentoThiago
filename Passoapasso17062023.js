bfe5635cba200dc7d293185a6a843042a9b02f22
Suelen Fernanda <suelenasbahr@gmail.com>
2023-06-17 15:53:32 -0300

    passo a passo 17062023

---
+++ Passoapasso17062023.js
@@ -0,0 +1,15 @@
+
+var altura = 1.54
+var peso = 38
+var imc = peso/(altura*altura)
+console.log ('o valor do imc é ' +imc)
+// Entre 18,5 e 24,99	Peso normal
+// Entre 25 e 29,99	Acima do peso
+// Entre 30 e 34,99	Obesidade I
+// Entre 35 e 39,99	Obesidade II (severa)
+
+
+//git status = vai verificar se possui alguma alteração no projeto(fica na cor vermelha)
+//git add . = significa que tudo que foi feito no projeto será salvo 
+//git commit -m 'jucdjncekjnewj' significa que vamos comitar o projeto ou seja mandar o mesmo para o git (programa)
+//git push significa recarregar a pagina com o projeto salvo
