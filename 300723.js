4eff190dca38f0a51b9c1d1b58a23fe3cbf67abb
Suelen Fernanda <suelenasbahr@gmail.com>
2023-07-23 19:53:29 -0300

    vestidos

---
+++ 300723.js
@@ -0,0 +1,8 @@
+var vestido1 = 290
+var vestido2 = 120
+console.log ('o vestido1 mais caro é:'+ vestido1 +'o vestido2 mais caro é '+ vestido2)
+
+//git status = vai verificar se possui alguma alteração no projeto(fica na cor vermelha)
+//git add . = significa que tudo que foi feito no projeto será salvo 
+//git commit -m 'jucdjncekjnewj' significa que vamos comitar o projeto ou seja mandar o mesmo para o git (programa)
+//git push significa recarregar a pagina com o projeto salvo
